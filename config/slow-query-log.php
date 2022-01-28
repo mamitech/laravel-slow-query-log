b7deb35297c99c95e4a1996e5b999d904f0671ed
arfn <arifin@mamiteam.com>
2022-01-29 03:04:19 +0700

    update default min threshold

--- config/slow-query-log.php
+++ config/slow-query-log.php
@@ -2,6 +2,6 @@
 
 return [
     'enabled' => (bool) env('SLOW_QUERY_LOG_ENABLED', false),
-    'min-threshold' => (int) env('SLOW_QUERY_MIN_THRESHOLD', 1), # in millisecond
+    'min-threshold' => (int) env('SLOW_QUERY_MIN_THRESHOLD', 50), # in millisecond
     'trace-only' => env('SLOW_QUERY_TRACE_ONLY', '')
 ];
