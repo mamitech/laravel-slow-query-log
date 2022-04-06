6d563243f67d80e05c83611e15bed550633bf245
arfn <arifin@mamiteam.com>
2022-04-06 12:09:39 +0700

    revert config change, add unit test run to Makefile

--- config/slow-query-log.php
+++ config/slow-query-log.php
@@ -2,11 +2,11 @@
 
 return [
     # set to true to enable slow query log
-    'enabled' => false,
+    'enabled' => (bool) env('SLOW_QUERY_LOG_ENABLED', false),
 
     # minimum millisecond a query execution is
     # considered as slow to be recorded
-    'min-threshold' => 50,
+    'min-threshold' => (int) env('SLOW_QUERY_MIN_THRESHOLD', 50), # in millisecond
 
     # sometimes tracelog could be too long due to
     # it includes all the third party library.
@@ -15,18 +15,18 @@ return [
     # log. For example, when you set trace-only to 'app'
     # stack trace in log will only include all files
     # with 'app' in it's path
-    'trace-only' => '',
+    'trace-only' => env('SLOW_QUERY_TRACE_ONLY', ''),
 
     # the opposite of trace-only, it will exclude all
     # file path that has the substring of its value
-    'trace-exclude' => '',
+    'trace-exclude' => env('SLOW_QUERY_TRACE_EXCLUDE', ''),
 
     # there are three option to store the slow query:
     # 1. file : all slow query log will be recorded in local file
     # 2. database : all slow query log will be recorded to a table in db
     # 3. channel : custom log handler channel.
     #      You can specify custom channels in 'channels' value in this config
-    'storage' => 'file',
+    'storage' => env('SLOW_QUERY_LOG_STORAGE', 'file'),
 
     'log-channel' => 'logstash'
 ];
